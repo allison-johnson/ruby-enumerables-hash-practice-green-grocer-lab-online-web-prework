8e589adb982a0de0b1e541e32e2aa385d56f4d75
Allison Johnson <allison.cuttler@gmail.com>
2019-10-09 02:21:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,21 @@
 def consolidate_cart(cart)
-  # code here
+  consol_cart = Hash.new
+  
+  cart.each do |item|
+    if consol_cart[item.keys[0]] #Item with that key is already in consol cart
+      consol_cart[item.keys[0]][:count] += 1
+      
+    else #Item with that key is NOT already in consol cart
+      item_name = item.keys[0]
+      new_consol_cart_item = {}
+      new_consol_cart_item[:price] = item[item_name][:price]
+      new_consol_cart_item[:clearance] = item[item_name][:clearance]
+      new_consol_cart_item[:count] = 1
+      consol_cart[item_name] = new_consol_cart_item
+  
+    end #end if
+  end #end each block
+  consol_cart
 end
 
 def apply_coupons(cart, coupons)
